f91aa4f2d02d3514a642f95f865133239285b435
Hani Abdulfattah <haniabdulfattah32@gmail.com>
2025-05-04 11:15:19 +0400

    starting with comments services and the client frontend

--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const {randomBytes} = require("crypto");
 const bodyParser = require("body-parser");
-const app = express(    );
+const app = express();
+const cors = require("cors");
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
